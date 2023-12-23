67a15ac4460a6a9aa140049e61797e0bca1ae8d4
patrick <patrick@patrickdeMacBook-Pro.local>
2023-12-23 14:04:00 +0800

    feat(middleware): add route guard

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,34 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
   publicRoutes: ["/"],
+  afterAuth(auth, req) {
+    // 登录且在公共route
+    if (auth.userId && auth.isPublicRoute) {
+      // 是否选择Org，选择就跳到对应的dashboard界面，否则就在选择界面
+      let path = "/select-org";
+      if (auth.orgId) {
+        path = `organization/${auth.orgId}`;
+      }
+      const orgSelection = new URL(path, req.url);
+      return NextResponse.redirect(orgSelection);
+    }
+    // 如果没有登录，则跳转到登录页面，且登录后自动返回之前想访问的页面
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({
+        returnBackUrl: req.url,
+      });
+    }
+    // 如果登录，但是没有org，且当前不在选择org就跳转到选择org
+    if (auth.userId && !auth.orgId && req.nextUrl.pathname !== "/select-org") {
+      const orgSelection = new URL("/select-org", req.url);
+      return NextResponse.redirect(orgSelection);
+    }
+  },
 });
 
 export const config = {
