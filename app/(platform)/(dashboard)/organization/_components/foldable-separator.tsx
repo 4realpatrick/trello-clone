a74acadc0a8c4ae901745e2746de93033ba5cc62
patrick <patrick@patrickdeMacBook-Pro.local>
2023-12-23 22:02:19 +0800

    refactor(organization.layout): change sidebar from resizable to foldable

---
+++ app/(platform)/(dashboard)/organization/_components/foldable-separator.tsx
@@ -0,0 +1,54 @@
+"use client";
+// Cmp
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { Separator } from "@/components/ui/separator";
+import {
+  DoubleArrowLeftIcon,
+  DoubleArrowRightIcon,
+} from "@radix-ui/react-icons";
+import { TooltipArrow } from "@radix-ui/react-tooltip";
+interface IFoldableSeparatorProps {
+  isFold: boolean;
+  onToggleFold: (fold: boolean) => void;
+}
+const FoldableSeparator: React.FC<IFoldableSeparatorProps> = ({
+  isFold,
+  onToggleFold,
+}) => {
+  return (
+    <Separator
+      orientation="vertical"
+      style={{ height: "inherit" }}
+      className="relative"
+      children={
+        <TooltipProvider delayDuration={200}>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <div
+                className="absolute top-[50%] w-6 h-6 border rounded-md flex items-center justify-center translate-x-[-50%] bg-background cursor-pointer"
+                onClick={() => onToggleFold(!isFold)}
+              >
+                {isFold ? (
+                  <DoubleArrowRightIcon className="text-primary" />
+                ) : (
+                  <DoubleArrowLeftIcon className="text-primary" />
+                )}
+              </div>
+            </TooltipTrigger>
+            <TooltipContent className="z-[100]">
+              <TooltipArrow className="!bg-background" />
+              <p>Toggle fold</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      }
+    />
+  );
+};
+
+export default FoldableSeparator;
--- app/(platform)/(dashboard)/organization/layout.tsx
+++ app/(platform)/(dashboard)/organization/layout.tsx
@@ -1,34 +1,28 @@
 "use client";
 // Cmp
-import {
-  ResizableHandle,
-  ResizablePanel,
-  ResizablePanelGroup,
-} from "@/components/ui/resizable";
 import Sidebar from "../_components/sidebar";
-import { useEffect, useState } from "react";
+import FoldableSeparator from "./_components/foldable-separator";
+// Hooks
+import { useState } from "react";
+// Utils
+import { cn } from "@/lib/utils";
 
 const OrganizationLayout = ({ children }: { children: React.ReactNode }) => {
-  const [isMounted, setIsMounted] = useState(false);
-  useEffect(() => {
-    setIsMounted(true);
-  }, []);
-  if (!isMounted) return null;
+  const [isFold, setIsFold] = useState(false);
   return (
     <main className="pt-20 md:pt-24 px-4 max-w-6xl 2xl:max-w-screen-xl mx-auto">
-      <ResizablePanelGroup direction="horizontal" className="flex gap-x-7">
-        <ResizablePanel
-          defaultSize={30}
-          minSize={27}
-          className="hidden md:block w-64 shrink-0"
+      <div className="flex gap-x-7">
+        <div
+          className={cn(
+            "w-64 shrink-0 hidden md:block transition-[width] overflow-hidden",
+            isFold && "w-0"
+          )}
         >
           <Sidebar />
-        </ResizablePanel>
-        <ResizableHandle withHandle className="hidden md:flex" />
-        <ResizablePanel defaultSize={70} minSize={40}>
-          {children}
-        </ResizablePanel>
-      </ResizablePanelGroup>
+        </div>
+        <FoldableSeparator isFold={isFold} onToggleFold={setIsFold} />
+        {children}
+      </div>
     </main>
   );
 };
