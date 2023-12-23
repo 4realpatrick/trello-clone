379372fe1e053bbc34558bf1f2d6f233c6e04673
patrick <patrick@patrickdeMacBook-Pro.local>
2023-12-23 17:59:47 +0800

    fix(organization.layout): solve another weird problem
    sidebar wide issue when browser refresh

--- app/(platform)/(dashboard)/organization/layout.tsx
+++ app/(platform)/(dashboard)/organization/layout.tsx
@@ -6,19 +6,23 @@ import {
   ResizablePanelGroup,
 } from "@/components/ui/resizable";
 import Sidebar from "../_components/sidebar";
+import { useEffect, useState } from "react";
 
 const OrganizationLayout = ({ children }: { children: React.ReactNode }) => {
+  const [isMounted, setIsMounted] = useState(false);
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+  if (!isMounted) return null;
   return (
-    <main className="mt-20 md:pt-24 px-4 max-w-6xl 2xl:max-w-screen-xl mx-auto">
+    <main className="pt-20 md:pt-24 px-4 max-w-6xl 2xl:max-w-screen-xl mx-auto">
       <ResizablePanelGroup direction="horizontal" className="flex gap-x-7">
         <ResizablePanel
           defaultSize={30}
           minSize={27}
-          className="hidden md:block"
+          className="hidden md:block w-64 shrink-0"
         >
-          <div className="w-64 shrink-0 hidden md:block">
-            <Sidebar />
-          </div>
+          <Sidebar />
         </ResizablePanel>
         <ResizableHandle withHandle className="hidden md:flex" />
         <ResizablePanel defaultSize={70} minSize={40}>
