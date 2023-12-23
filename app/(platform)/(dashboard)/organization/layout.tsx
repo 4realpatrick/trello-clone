8e7cdfc8819b2e9130cf576853127e6211e932d5
patrick <patrick@patrickdeMacBook-Pro.local>
2023-12-23 16:38:40 +0800

    fix(organization.layout): ResizableHandle shouldn't show at mobile

--- app/(platform)/(dashboard)/organization/layout.tsx
+++ app/(platform)/(dashboard)/organization/layout.tsx
@@ -11,12 +11,16 @@ const OrganizationLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="mt-20 md:pt-24 px-4 max-w-6xl 2xl:max-w-screen-xl mx-auto">
       <ResizablePanelGroup direction="horizontal" className="flex gap-x-7">
-        <ResizablePanel defaultSize={30} minSize={27}>
+        <ResizablePanel
+          defaultSize={30}
+          minSize={27}
+          className="hidden md:block"
+        >
           <div className="w-64 shrink-0 hidden md:block">
             <Sidebar />
           </div>
         </ResizablePanel>
-        <ResizableHandle withHandle />
+        <ResizableHandle withHandle className="hidden md:flex" />
         <ResizablePanel defaultSize={70} minSize={40}>
           {children}
         </ResizablePanel>
