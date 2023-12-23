2f21ed57fe977f02eff2dce63f2dc55642cc30d3
patrick <patrick@patrickdeMacBook-Pro.local>
2023-12-23 16:15:47 +0800

    feat(organization.layout): make sidebar resizable

---
+++ app/(platform)/(dashboard)/organization/layout.tsx
@@ -0,0 +1,28 @@
+"use client";
+// Cmp
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import Sidebar from "../_components/sidebar";
+
+const OrganizationLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="mt-20 md:pt-24 px-4 max-w-6xl 2xl:max-w-screen-xl mx-auto">
+      <ResizablePanelGroup direction="horizontal" className="flex gap-x-7">
+        <ResizablePanel defaultSize={30} minSize={27}>
+          <div className="w-64 shrink-0 hidden md:block">
+            <Sidebar />
+          </div>
+        </ResizablePanel>
+        <ResizableHandle withHandle />
+        <ResizablePanel defaultSize={70} minSize={40}>
+          {children}
+        </ResizablePanel>
+      </ResizablePanelGroup>
+    </main>
+  );
+};
+
+export default OrganizationLayout;
