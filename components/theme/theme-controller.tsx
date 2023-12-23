f8fe687f932ef2fd20ad7ddf39deacceee53e2dc
patrick <patrick@patrickdeMacBook-Pro.local>
2023-12-23 17:07:15 +0800

    fix(theme-controller): dropdown position error at wide screen

--- components/theme/theme-controller.tsx
+++ components/theme/theme-controller.tsx
@@ -35,34 +35,62 @@ export function ThemeController() {
   }, [curTheme]);
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild className="hidden md:block">
-        <Button>Theme</Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuTrigger asChild className="md:hidden">
-        <ColorWheelIcon className="text-primary w-4 h-4" />
-      </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-30">
-        <DropdownMenuRadioGroup
-          value={curTheme}
-          onValueChange={(val: string) => setCurTheme(val as TTheme)}
-        >
-          {THEME_ARRAY.map((theme) => (
-            <DropdownMenuRadioItem
-              value={theme}
-              className="justify-between"
-              key={theme}
-            >
-              <span>
-                {theme.slice(0, 1).toUpperCase() + theme.slice(1).toLowerCase()}
-              </span>
-              <div
-                className={`inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent bg-[${theme}] hover:bg-primary/80 rotate-90 justify-end`}
-              ></div>
-            </DropdownMenuRadioItem>
-          ))}
-        </DropdownMenuRadioGroup>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild className="md:hidden">
+          <ColorWheelIcon className="text-primary w-4 h-4" />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className="w-30">
+          <DropdownMenuRadioGroup
+            value={curTheme}
+            onValueChange={(val: string) => setCurTheme(val as TTheme)}
+          >
+            {THEME_ARRAY.map((theme) => (
+              <DropdownMenuRadioItem
+                value={theme}
+                className="justify-between"
+                key={theme}
+              >
+                <span>
+                  {theme.slice(0, 1).toUpperCase() +
+                    theme.slice(1).toLowerCase()}
+                </span>
+                <div
+                  className={`inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent bg-[${theme}] hover:bg-primary/80 rotate-90 justify-end`}
+                ></div>
+              </DropdownMenuRadioItem>
+            ))}
+          </DropdownMenuRadioGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild className="hidden md:block">
+          <Button>Theme</Button>
+        </DropdownMenuTrigger>
+
+        <DropdownMenuContent className="w-30">
+          <DropdownMenuRadioGroup
+            value={curTheme}
+            onValueChange={(val: string) => setCurTheme(val as TTheme)}
+          >
+            {THEME_ARRAY.map((theme) => (
+              <DropdownMenuRadioItem
+                value={theme}
+                className="justify-between"
+                key={theme}
+              >
+                <span>
+                  {theme.slice(0, 1).toUpperCase() +
+                    theme.slice(1).toLowerCase()}
+                </span>
+                <div
+                  className={`inline-flex items-center rounded-full border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent bg-[${theme}] hover:bg-primary/80 rotate-90 justify-end`}
+                ></div>
+              </DropdownMenuRadioItem>
+            ))}
+          </DropdownMenuRadioGroup>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
   );
 }
