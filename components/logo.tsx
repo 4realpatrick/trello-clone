50822911adae33111cfb41e13762bdcafb2edbd0
patrick <patrick@patrickdeMacBook-Pro.local>
2023-12-23 22:21:15 +0800

    refactor(logo): set Logo image high priority

--- components/logo.tsx
+++ components/logo.tsx
@@ -14,7 +14,7 @@ const Logo: React.FC<ILogoProps> = ({ size = 30 }) => {
   return (
     <Link href="/">
       <div className="hover:opacity-75 transition items-center gap-x-2 hidden md:flex">
-        <Image src="/logo.svg" alt="Logo" height={size} width={size} />
+        <Image src="/logo.svg" alt="Logo" height={size} width={size} priority />
         <p
           className={cn("text-lg text-neutral-700 pb-1", headingFont.className)}
         >
