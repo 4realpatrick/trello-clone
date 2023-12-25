603e35a0209d6485398112a1007faa8e7d83852f
patrick <patrick@patrickdeMacBook-Pro.local>
2023-12-25 16:01:02 +0800

    feat(organizationId.layout): add title change

--- app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/layout.tsx
@@ -1,6 +1,15 @@
 // Cmp
 import { OrgControl } from "./_components/org-control";
+// Utils
+import startCase from "lodash/startCase";
+import { auth } from "@clerk/nextjs";
 
+export async function generateMetadata() {
+  const { orgSlug } = auth();
+  return {
+    title: startCase(orgSlug || "organization"),
+  };
+}
 const OrganizationIdLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
