670e584d673794876e7bc9278cd81196ecbfec7e
patrick <patrick@patrickdeMacBook-Pro.local>
2023-12-26 21:38:59 +0800

    feat(board-list): add empty state for board-list

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -1,12 +1,14 @@
 // Cmp
+import FormDialog from "@/components/form/form-dialog";
 import FormPopover from "@/components/form/form-popover";
 import Hint from "@/components/hint";
 import { Skeleton } from "@/components/ui/skeleton";
-import { db } from "@/lib/db";
-import { auth } from "@clerk/nextjs";
 import { HelpCircle, User2 } from "lucide-react";
 import Link from "next/link";
+// Function
 import { redirect } from "next/navigation";
+import { auth } from "@clerk/nextjs";
+import { db } from "@/lib/db";
 
 const BoardList = async () => {
   const { orgId } = auth();
@@ -27,34 +29,42 @@ const BoardList = async () => {
         <User2 className="size-6 mr-2" />
         Your boards
       </div>
-      <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
-        {boards.map((board) => (
-          <Link
-            href={`/board/${board.id}`}
-            key={board.id}
-            style={{ backgroundImage: `url(${board.imageThumbUrl})` }}
-            className="group relative aspect-video bg-no-repeat bg-center bg-cover bg-primary rounded-sm size-full p-2 overflow-hidden"
-          >
-            <div className="absolute inset-0 group-hover:bg-foreground/40 transition" />
-            <p className="relative font-semibold text-white">{board.title}</p>
-          </Link>
-        ))}
-        <FormPopover sideOffset={10} side="right">
-          <div
-            role="button"
-            className="aspect-video relative size-full bg-muted rounded-sm flex flex-col gap-y1 items-center justify-center hover:opacity-75 transition"
-          >
-            <p className="text-sm">Create new board</p>
-            <span className="text-xs">5 remaining</span>
-            <Hint
-              sideOffset={40}
-              descrption={`Free Workspaces can have up to 5 open boards. For unlimited boards upgrade this workspace.`}
-            >
-              <HelpCircle className="absolute bottom-2 right-2 size-[14px]" />
-            </Hint>
+      {boards.length === 0 ? (
+        <FormDialog>
+          <div className="flex h-48 md:h-64 items-center justify-center rounded-md border border-dashed text-xl hover:border-primary cursor-pointer transition hover:text-primary">
+            No board here, click to create
           </div>
-        </FormPopover>
-      </div>
+        </FormDialog>
+      ) : (
+        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+          {boards.map((board) => (
+            <Link
+              href={`/board/${board.id}`}
+              key={board.id}
+              style={{ backgroundImage: `url(${board.imageThumbUrl})` }}
+              className="group relative aspect-video bg-no-repeat bg-center bg-cover bg-primary rounded-sm size-full p-2 overflow-hidden"
+            >
+              <div className="absolute inset-0 group-hover:bg-foreground/40 transition" />
+              <p className="relative font-semibold text-white">{board.title}</p>
+            </Link>
+          ))}
+          <FormPopover sideOffset={10} side="right">
+            <div
+              role="button"
+              className="aspect-video relative size-full bg-muted rounded-sm flex flex-col gap-y1 items-center justify-center hover:opacity-75 transition"
+            >
+              <p className="text-sm">Create new board</p>
+              <span className="text-xs">5 remaining</span>
+              <Hint
+                sideOffset={40}
+                descrption={`Free Workspaces can have up to 5 open boards. For unlimited boards upgrade this workspace.`}
+              >
+                <HelpCircle className="absolute bottom-2 right-2 size-[14px]" />
+              </Hint>
+            </div>
+          </FormPopover>
+        </div>
+      )}
     </div>
   );
 };
