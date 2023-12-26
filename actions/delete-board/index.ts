2c5559bb3ca38651486fe67893e766f8e66beaa2
patrick <patrick@patrickdeMacBook-Pro.local>
2023-12-26 15:36:55 +0800

    feat(server-action): delete-board action init

---
+++ actions/delete-board/index.ts
@@ -0,0 +1,37 @@
+"use server";
+
+import { auth } from "@clerk/nextjs";
+import { TInputType, TReturnType } from "./types";
+import { db } from "@/lib/db";
+import { revalidatePath } from "next/cache";
+import { createSafeAction } from "@/lib/create-safe-action";
+import { DeleteBoard } from "./schema";
+import { redirect } from "next/navigation";
+
+const handler = async (data: TInputType): Promise<TReturnType> => {
+  const { userId, orgId } = auth();
+  if (!userId || !orgId) {
+    return {
+      error: "Unauthorized",
+    };
+  }
+  const { id } = data;
+  let board;
+  try {
+    board = await db.board.delete({
+      where: {
+        id,
+        orgId,
+      },
+    });
+  } catch (error) {
+    console.log("Internal Error Found in delete-board", error);
+    return {
+      error: "Failed to delete",
+    };
+  }
+  revalidatePath(`/organization/${orgId}`);
+  redirect(`/organization/${orgId}`);
+};
+
+export const deleteBoard = createSafeAction(DeleteBoard, handler);
