5513cb6bd27d9a46a9d18e31f5422e171c29c812
patrick <patrick@patrickdeMacBook-Pro.local>
2023-12-27 14:06:38 +0800

    feat(boardId.page): page layout finished

---
+++ app/(platform)/(dashboard)/board/[boardId]/page.tsx
@@ -0,0 +1,46 @@
+// Cmp
+import ListContainer from "./_components/list/list-container";
+// Function
+import { db } from "@/lib/db";
+import { auth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+// Types
+interface IBoardIdPageProps {
+  params: {
+    boardId: string;
+  };
+}
+
+const BoardIdPage = async ({ params: { boardId } }: IBoardIdPageProps) => {
+  const { orgId } = auth();
+  if (!orgId) {
+    redirect("/select-org");
+  }
+  const lists = await db.list.findMany({
+    where: {
+      boardId,
+      board: {
+        orgId,
+      },
+    },
+    include: {
+      cards: {
+        orderBy: {
+          order: "asc",
+        },
+      },
+    },
+    orderBy: {
+      order: "asc",
+    },
+  });
+  console.log("data:", lists);
+
+  return (
+    <div className="p-4 h-full overflow-x-auto">
+      <ListContainer boardId={boardId} data={lists} />
+    </div>
+  );
+};
+
+export default BoardIdPage;
