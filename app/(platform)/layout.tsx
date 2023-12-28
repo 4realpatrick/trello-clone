8e15d6f6a1a04973e2d7ffd916baa2bf33a9611b
patrick <patrick@patrickdeMacBook-Pro.local>
2023-12-28 18:41:44 +0800

    feat(react-query): add provider for layout

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -1,12 +1,17 @@
 // Cmp
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/sonner";
+import ModalProvider from "@/components/modals/provider/modal-provider";
+import { QueryProvider } from "@/components/modals/provider/query-provider";
 
 const PlatFormLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <ClerkProvider>
-      <Toaster richColors />
-      {children}
+      <QueryProvider>
+        <Toaster richColors />
+        <ModalProvider />
+        {children}
+      </QueryProvider>
     </ClerkProvider>
   );
 };
