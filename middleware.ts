ef643c3f9eec473b378fb9106109c342683fd4b2
patrick <patrick@patrickdeMacBook-Pro.local>
2023-12-31 10:10:44 +0800

    feat(stripe): add a webhook

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,7 @@ import { NextResponse } from "next/server";
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
-  publicRoutes: ["/"],
+  publicRoutes: ["/", "/api/webhook"],
   afterAuth(auth, req) {
     // 登录且在公共route
     if (auth.userId && auth.isPublicRoute) {
